8a759bcf822c3241214f70c9d13ebeed3c33d26e
lavesh4807 <142747980+lavesh4807@users.noreply.github.com>
2024-09-06 10:26:50 +0530

    task.ino

--- task.ino
+++ task.ino
@@ -1,42 +1,51 @@
 #include "GPIO_DEAKIN.h"
 #include "sam.h"
 
-bool GPIO_DEAKIN::Config_GPIO(char PortNum, char PinNum, char Mode) {
-    // Determine the port
-    PortGroup *port;
-    if (PortNum == 'A') {
-        port = &PORT->Group[0];
-    } else if (PortNum == 'B') {
-        port = &PORT->Group[1];
-    } else {
+enum PinMode {
+    INPUT_MODE,
+    OUTPUT_MODE
+};
+
+enum PinState {
+    LOW_STATE,
+    HIGH_STATE
+};
+
+bool validatePortAndPin(char PortNum, char PinNum) {
+    if ((PortNum != 'A' && PortNum != 'B') || PinNum < 0 || PinNum > 31) {
         return false;
     }
-    
-    // Configure pin as input or output
-    if (Mode == OUTPUT) {
+    return true;
+}
+
+bool GPIO_DEAKIN::Config_GPIO(char PortNum, char PinNum, PinMode mode) {
+    if (!validatePortAndPin(PortNum, PinNum)) {
+        return false;
+    }
+
+    PortGroup *port = (PortNum == 'A') ? &PORT->Group[0] : &PORT->Group[1];
+
+    if (mode == OUTPUT_MODE) {
         port->DIRSET.reg = (1 << PinNum);
-    } else if (Mode == INPUT) {
+    } else if (mode == INPUT_MODE) {
         port->DIRCLR.reg = (1 << PinNum);
     } else {
         return false;
     }
-    
+
     return true;
 }
 
-bool GPIO_DEAKIN::Write_GPIO(char PortNum, char PinNum, bool State) {
-    PortGroup *port;
-    if (PortNum == 'A') {
-        port = &PORT->Group[0];
-    } else if (PortNum == 'B') {
-        port = &PORT->Group[1];
-    } else {
+bool GPIO_DEAKIN::Write_GPIO(char PortNum, char PinNum, PinState state) {
+    if (!validatePortAndPin(PortNum, PinNum)) {
         return false;
     }
 
-    if (State == HIGH) {
+    PortGroup *port = (PortNum == 'A') ? &PORT->Group[0] : &PORT->Group[1];
+
+    if (state == HIGH_STATE) {
         port->OUTSET.reg = (1 << PinNum);
-    } else if (State == LOW) {
+    } else if (state == LOW_STATE) {
         port->OUTCLR.reg = (1 << PinNum);
     } else {
         return false;
@@ -46,14 +55,11 @@ bool GPIO_DEAKIN::Write_GPIO(char PortNum, char PinNum, bool State) {
 }
 
 bool GPIO_DEAKIN::Read_GPIO(char PortNum, char PinNum) {
-    PortGroup *port;
-    if (PortNum == 'A') {
-        port = &PORT->Group[0];
-    } else if (PortNum == 'B') {
-        port = &PORT->Group[1];
-    } else {
+    if (!validatePortAndPin(PortNum, PinNum)) {
         return false;
     }
+
+    PortGroup *port = (PortNum == 'A') ? &PORT->Group[0] : &PORT->Group[1];
 
     return (port->IN.reg & (1 << PinNum)) != 0;
 }
